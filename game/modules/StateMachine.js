a6603b65eeef1e334660f03fd76b4fe3479759cc
Kenny Baron <kennybaron@fastmail.com>
2022-11-22 21:03:31 -0500

    add statemachine class

---
+++ game/modules/StateMachine.js
@@ -0,0 +1,28 @@
+export default class StateMachine {
+  constructor(states, game) {
+    this.states = states
+    this.current = {
+      render() {},
+      update() {},
+      enter() {},
+      exit() {}
+    }
+    this.game = game
+  }
+
+  change(stateName, params) {
+    if (!Object.keys(this.states).includes(stateName)) {
+      throw Error('Cannot change state, invalid state name')
+    }
+    this.current.exit()
+    this.current = new this.states[stateName](this.game, params)
+  }
+
+  update(t) {
+    this.current.update(t)
+  }
+
+  render(t) {
+    this.current.render(t)
+  }
+}
