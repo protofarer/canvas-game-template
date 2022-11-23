54804f17243149631fee06e0f229b419b9625116
Kenny Baron <kennybaron@fastmail.com>
2022-11-22 21:42:06 -0500

    update traits, remove package-lock

--- game/ents/Traits.js
+++ game/ents/Traits.js
@@ -30,33 +30,33 @@ export default {
     secondaryColor: ''
   },
 
-  // Apple: {
-  //   r: 6,
-  //   baseExp: 10,
-  //   primaryColor: {
-  //     hueStart: 0, 
-  //     hueEnd: 25, 
-  //     satStart: 70,
-  //     satEnd: 30,
-  //     lumStart: 50,
-  //     lumEnd: 25,
-  //   },
-  //   secondaryColor: 'hsl(95, 60%, 50%)',
-  //   chompEffectWord: Constants.collisionFunction.BASE_CHOMP,
-  //   digestion: {
-  //     timeLeft: 5000,
-  //     baseTime: 5000
-  //   },
-  //   postDigestionData: [
-  //     {
-  //       effect: 'moveSpeed',
-  //       type: 'offset',
-  //       moveSpeed: 0.25,
-  //       duration: 8000,
-  //       timeLeft: 8000
-  //     }
-  //   ],
-  // },
+  Apple: {
+    r: 6,
+    baseExp: 10,
+    primaryColor: {
+      hueStart: 0, 
+      hueEnd: 25, 
+      satStart: 70,
+      satEnd: 30,
+      lumStart: 50,
+      lumEnd: 25,
+    },
+    secondaryColor: 'hsl(95, 60%, 50%)',
+    chompEffectWord: Constants.collisionFunction.BASE_CHOMP,
+    digestion: {
+      timeLeft: 5000,
+      baseTime: 5000
+    },
+    postDigestionData: [
+      {
+        effect: 'moveSpeed',
+        type: 'offset',
+        moveSpeed: 0.25,
+        duration: 8000,
+        timeLeft: 8000
+      }
+    ],
+  },
 
   Mob: {
     headingDegrees: -90,
@@ -69,52 +69,52 @@ export default {
     minTurnRate: 0,
   },
 
-  // Ant: {
-  //   r: 4,
-  //   baseExp: 10,
-  //   primaryColor: {
-  //     hueStart: 0,
-  //     hueEnd: 0,
-  //     satStart: 0,
-  //     satEnd: 0,
-  //     lumStart: 0,
-  //     lumEnd: 0,
-  //     alphaStart: 1,
-  //     alphaEnd: 0.2
-  //   },
-  //   baseMoveSpeed: 2,
-  //   turnRateOffset: 5,
-  //   swallowables: [
-  //     'apple', 
-  //     'mango', 
-  //     'banana'
-  //   ],
-  // },
+  Ant: {
+    r: 4,
+    baseExp: 10,
+    primaryColor: {
+      hueStart: 0,
+      hueEnd: 0,
+      satStart: 0,
+      satEnd: 0,
+      lumStart: 0,
+      lumEnd: 0,
+      alphaStart: 1,
+      alphaEnd: 0.2
+    },
+    baseMoveSpeed: 2,
+    turnRateOffset: 5,
+    swallowables: [
+      'apple', 
+      'mango', 
+      'banana'
+    ],
+  },
 
-  // Snek: {
-  //   r: 10,
-  //   hitOffset: 4,
-  //   baseExp: 10,
-  //   level: 3,
-  //   levelMultiplier: 2,
-  //   segLevelMultiplier: 1.25,
-  //   baseSegmentCount: 3,
-  //   basePrimaryColor: `hsl(100, 100%, 32%)`,
-  //   colorTongue: 'red',
-  //   colorEyeWhites: 'white',
-  //   colorFangs: 'white',
-  //   colorLeftEye: 'hsl(55, 100%, 25%)',
-  //   colorRightEye: 'hsl(230, 100%, 80%)',
-  //   baseMoveSpeed: 2,
-  //   turnRateOffset: 4,
-  //   swallowables: [ 'apple', 'mango', 'ant', 'pebble', 'banana' ],
-  //   enemySpecies: ['centipede'],
-  //   effectPanic: {
-  //     effect: 'panic',
-  //     moveSpeed: 2,
-  //     turnRate: 10,
-  //     timeLeft: 4000,
-  //     duration: 4000
-  //   },
-  // },
+  Snek: {
+    r: 10,
+    hitOffset: 4,
+    baseExp: 10,
+    level: 3,
+    levelMultiplier: 2,
+    segLevelMultiplier: 1.25,
+    baseSegmentCount: 3,
+    basePrimaryColor: `hsl(100, 100%, 32%)`,
+    colorTongue: 'red',
+    colorEyeWhites: 'white',
+    colorFangs: 'white',
+    colorLeftEye: 'hsl(55, 100%, 25%)',
+    colorRightEye: 'hsl(230, 100%, 80%)',
+    baseMoveSpeed: 2,
+    turnRateOffset: 4,
+    swallowables: [ 'apple', 'mango', 'ant', 'pebble', 'banana' ],
+    enemySpecies: ['centipede'],
+    effectPanic: {
+      effect: 'panic',
+      moveSpeed: 2,
+      turnRate: 10,
+      timeLeft: 4000,
+      duration: 4000
+    },
+  },
 }
